e85efad168abc160f19e7eceaf45d8e1bb488183
Norayr <asoyannorayr99@gmail.com>
2023-11-11 14:55:34 +0300

    add

--- src/MiddIeOfArray.java
+++ src/MiddIeOfArray.java
@@ -6,21 +6,17 @@ public class MiddIeOfArray {
         int[] result = getArrayMiddle(numbers);
         System.out.println(Arrays.toString(result));
 
-}
+    }
+
     public static int[] getArrayMiddle(int[] numbers) {
-        if (numbers.length > 2) {
-            if (numbers.length % 2 == 0) {
-                int[] arr = new int[5];
-                arr[0] = numbers[numbers.length / 2 - 1];
-                arr[1] = numbers[numbers.length / 2];
-                return arr;
-            } else {
-                int[] arr1 = new int[1];
-                arr1[0] = numbers[numbers.length / 2];
-                return arr1;
-            }
-            }else {
-                return numbers;
-            }
+        if (numbers.length == 0) {
+            return new int[0];
+        }
+        int start = numbers.length / 2;
+        if (numbers.length % 2 == 0) {
+            return new int[]{numbers[start - 1], numbers[start]};
+        } else {
+            return new int[]{numbers[start]};
         }
     }
+}
